2329e0e6369031ad1cde92fd5ac187240e1cbbd1
Marcin Fundalewicz <marcin.fundalewicz@gmail.com>
2024-06-10 20:39:41 +0200

    Rozwiazanie zadania

---
+++ src/main/java/pl/javastart/jjdind84_marcinfundalewicz_zad_27/PersonRepository.java
@@ -0,0 +1,6 @@
+package pl.javastart.jjdind84_marcinfundalewicz_zad_27;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
---
+++ src/main/resources/db/testdata/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO person (first_name, last_name, birth_date, email, is_signed_up_for_newsletter)
+VALUES  ('Marcin', 'Fundalewicz', '1998-09-06', 'marcin.fundalewiczk@gmail.com', true),
+        ('Sławek', 'Ludwiczak', '1988-09-06', 'slawek.ludwiczak@gmail.com', false),
+        ('Marcin', 'Kunert', '1987-09-06', 'marcin.kunert@gmail.com', true);
