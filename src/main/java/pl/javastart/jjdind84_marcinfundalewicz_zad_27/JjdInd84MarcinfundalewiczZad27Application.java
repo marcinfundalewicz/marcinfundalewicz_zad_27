7445e5cb02753fa7446980a20c3e3409cb4c5004
Marcin Fundalewicz <marcin.fundalewicz@gmail.com>
2024-06-10 18:43:55 +0200

    Stworzenie projektu

---
+++ src/main/java/pl/javastart/jjdind84_marcinfundalewicz_zad_27/JjdInd84MarcinfundalewiczZad27Application.java
@@ -0,0 +1,13 @@
+package pl.javastart.jjdind84_marcinfundalewicz_zad_27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjdInd84MarcinfundalewiczZad27Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JjdInd84MarcinfundalewiczZad27Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/javastart/jjdind84_marcinfundalewicz_zad_27/JjdInd84MarcinfundalewiczZad27ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.jjdind84_marcinfundalewicz_zad_27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjdInd84MarcinfundalewiczZad27ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
